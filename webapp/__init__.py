6dbd8a9835d504dcc08f48cafc045889ee37bdf2
andrehaffner <andrehaffner@outlook.com>
2023-06-20 22:20:12 -0700

    database uri

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -10,7 +10,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv("PostgresURI")
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv("PostgresURI") + "flask_andrehaffner"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = "as@d145!sidh12*G3478"
     app.config['SEND_FILE_MAX_AGE'] = 0
