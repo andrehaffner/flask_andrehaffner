22f56c4ca5ce159f5267490397508bedb4705e8a
Andre Haffner <andrehaffner@outlook.com>
2024-01-14 22:02:32 -0800

    new flask no database

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,20 +1,10 @@
-from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
 from flask import Flask
-from os import getenv
-
-
-load_dotenv()
-db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv("PostgresURI") + "flask_andrehaffner"
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = "as@d145!sidh12*G3478"
     app.config['SEND_FILE_MAX_AGE'] = 0
     from .views import Views
     app.register_blueprint(Views, url_prefix='/')
-    db.init_app(app)
     return app
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,3 @@
-from .models import Experience, Education, Certificate
 from flask import Blueprint, render_template
 
 
@@ -12,30 +11,14 @@ def home():
 
 @Views.route("/professional")
 def professional():
-    data = Experience.query.order_by(Experience.id).all()
-    for experience in data:
-        experience.description = experience.description.split("<br>")
-    return render_template("professional.html", data=data)
+    return render_template("professional.html")
 
 
 @Views.route("/education")
 def education():
-    data = Education.query.order_by(Education.id).all()
-    for education in data:
-        education.description = education.description.split("<br>")
-    return render_template("education.html", data=data)
+    return render_template("education.html")
 
 
 @Views.route("/certificates")
 def certificates():
-    data = Certificate.query.order_by(Certificate.id).all()
-    return render_template("certificates.html", data=data)
-
-
-@Views.route("/data")
-def data_page():
-    professional_table = Experience.query.order_by(Experience.id).all()
-    education_table = Education.query.order_by(Education.id).all()
-    certificates_table = Certificate.query.order_by(Certificate.id).all()
-    return render_template("data_page.html", professional_table=professional_table,
-                           education_table=education_table, certificates_table=certificates_table)
+    return render_template("certificates.html")
